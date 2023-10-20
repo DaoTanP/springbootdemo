b37ca58c074ae0b7d044a42f93baa1648fd1b84e
vantrong <vantrong@example.com>
2023-10-20 09:57:29 +0700

    add privateApi that user with jwt token can access

--- src/main/java/demoproject/springbootdemo/controllers/DemoController.java
+++ src/main/java/demoproject/springbootdemo/controllers/DemoController.java
@@ -1,13 +1,29 @@
 package demoproject.springbootdemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import demoproject.springbootdemo.utils.JwtUtils;
+
 @RestController
 public class DemoController {
+    @Autowired
+    private JwtUtils jwtUtils;
+
     @RequestMapping("/")
     public String index() {
         return "index page";
+    }
+
+    @RequestMapping("/privateApi")
+    public ResponseEntity<String> privateApi(@RequestHeader(value = "authorization", defaultValue = "") String auth) {
+        if(jwtUtils.verify(auth) != null)
+            return ResponseEntity.status(200).body("Private access");
+        
+            return ResponseEntity.status(401).body("Access denied");
     }
 
     @RequestMapping("/home")
--- src/main/java/demoproject/springbootdemo/utils/JwtUtils.java
+++ src/main/java/demoproject/springbootdemo/utils/JwtUtils.java
@@ -41,13 +41,13 @@ public class JwtUtils {
                 .compact();
     }
 
-    public Claims verify(String authorization) throws Exception {
+    public Claims verify(String authorization) {
 
         try {
             Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authorization).getBody();
             return claims;
         } catch (Exception e) {
-            throw e;
+            return null;
         }
 
     }
